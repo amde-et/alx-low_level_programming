8076bddfcb271456bdd042acc241134b2e56e5d9
lightpillar <amdecourse@gmail.com>
2022-03-11 00:29:28 +0300

    final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ *main - Entry point
+ *Return: Always 1
  *
- * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
 
-	return (1);
-}:
+
