f1574299fb43893ec90e5bf66f6d6d639853d047
lightpillar <amdecourse@gmail.com>
2022-03-16 01:26:39 +0300

    try 1 2 3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
-* main - This function prints the pharse '_putchar' to the StndOut
-* The variable 'a' sets counter from 0
-* The variable 'b' defines an array of the letters that are going to be written
-* Return: Always 0 (Success)
-**/
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
 int main(void)
 {
-int a = 0;
-char b[] = "_putchar/n";
-for (a = 0; a < 11; a++)
-{
-_putchar(b[a]);
-}
-return (0);
+	char ermias[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(ermias[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
-* print_alphabet - Function to print  uppearcase letters from 'a' to 'z'
-* This cicle defines variables in ASCII code format
-* Return: Always 0 (Success)
+* print_alphabet - prints alphabet in lowercase
+*
 **/
 void print_alphabet(void)
 {
-int az;
-for (az = 97; az < 123; az++)
-{
-_putchar(az);
-}
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
 _putchar('\n');
 }
