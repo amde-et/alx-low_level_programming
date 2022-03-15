be43bab99fd4ddf13c942f14e3557f07acfe7fe0
lightpillar <amdecourse@gmail.com>
2022-03-16 02:25:44 +0300

    edit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
- 
+
 int main()
 {
    unsigned long long n, first = 0, second = 1, next, c;
- 
+
    printf("Enter the number of terms\n");
    scanf("%d",&n);
- 
+
    printf("First %d terms of Fibonacci series are :-\n",n);
- 
+
    for ( c = 0 ; c < n ; c++ )
    {
       if ( c <= 1 )
@@ -21,6 +21,7 @@ int main()
       }
       printf("%d\n",next);
    }
- 
+
    return 0;
 }
+
