e00e162624e0d89d7f430080e24b039343314011
lightpillar <amdecourse@gmail.com>
2022-03-15 15:50:29 +0300

    try 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
-int main(){
-	printf("_putchar\n"
-			}
-			return 0;
-
+#include "main.h"
+/**
+* main - This function prints the pharse '_putchar' to the StndOut
+* The variable 'a' sets counter from 0
+* The variable 'b' defines an array of the letters that are going to be written
+* Return: Always 0 (Success)
+**/
+int main(void)
+{
+int a = 0;
+char b[] = "_putchar/n";
+for (a = 0; a < 11; a++)
+{
+_putchar(b[a]);
+}
+return (0);
+}
