5bb74979e4f2893c2165e11a5a2ea18a9b0ec1b0
lightpillar <amdecourse@gmail.com>
2022-03-18 00:25:51 +0300

    updated

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line -  checks for checks for a digit (0 through 9).
+ * @n: n -  Variable
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 1; x <= n; x++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
+
