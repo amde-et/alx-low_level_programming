bd73eb2003b3b28ced0beca2664738380b71f10d
lightpillar <amdecourse@gmail.com>
2022-03-18 00:06:47 +0300

    supper c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
+
