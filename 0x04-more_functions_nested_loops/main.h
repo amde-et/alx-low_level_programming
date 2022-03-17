ee44d91dc7a08e2cbb88d00a40e11219908423d6
lightpillar <amdecourse@gmail.com>
2022-03-18 00:02:32 +0300

    main

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month,int day);
+void print_remaining_days(int month,int day,int year);
+void print_to_98(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
