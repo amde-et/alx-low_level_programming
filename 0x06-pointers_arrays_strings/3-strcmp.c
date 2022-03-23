8e9b553c12c01a4ffb7957f1c13b9e7007166912
amde-et <99601438+amde-et@users.noreply.github.com>
2022-03-23 23:04:58 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,13 @@
 #include "main.h"
 /**
-* here 
-*i
-*try
-*/
+ *_strcmp - compares two strings.
+ *@s1: pointer to first string.
+ *@s2: pointer to second string.
+ *
+ *Return: value less than 0 if string is less than the other.
+ *value greater than 0 if string is greater than the other.
+ *0 if strings are equal.
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int counter, cmpVal;
