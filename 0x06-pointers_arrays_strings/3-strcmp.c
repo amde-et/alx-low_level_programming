9895ddb797ac8c77f4d46bc131c324f9e7f470cd
amde-et <99601438+amde-et@users.noreply.github.com>
2022-03-23 23:02:38 +0300

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ 
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int counter, cmpVal;
+
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
+	{
+		counter++;
+	}
+
+	cmpVal = s1[counter] - s2[counter];
+	return (cmpVal);
+}
