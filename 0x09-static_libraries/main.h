0e0b076e23cf81f241f2e9bdbc0d772c32ff981e
lightpillar <amdecourse@gmail.com>
2022-04-02 00:11:33 +0300

    main

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,4 +16,5 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, charif
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
