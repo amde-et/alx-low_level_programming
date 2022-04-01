5e5c54323dc80593d340d510f88b9c5509be0c89
lightpillar <amdecourse@gmail.com>
2022-04-02 02:02:04 +0300

    work

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
-=======
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
->>>>>>> 1e0db2cadd13378defded490224ae86f5506fd3c
+gcc -c *.c
+ar rc liball.a *.o
+rm *.o
