ba82d8b4ed044e5b2de6ddc631d4c6e6c0e533c2
amde-et <99601438+amde-et@users.noreply.github.com>
2022-04-28 17:46:50 +0300

    updated

---
+++ 0-binary_to_uint.c/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_listint2 - Will free a linked list
+* @head: The head of the linked list
+**/
+
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+if (head == NULL)
+return;
+while (*head)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
+}
+*head = NULL;
+}
